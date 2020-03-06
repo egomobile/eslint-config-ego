1eb1fc301e7d3673ad432f5a5f48be25bda52b58
Marcel Kloubert <marcel.kloubert@e-go-digital.com>
2020-03-06 18:14:24 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# eslint-config-ego
+
+> ESLint [shareable config](http://eslint.org/docs/developer-guide/shareable-configs.html) for the [e.GO style guide](https://www.e-go-digital.com/)
+
+
+## Installation
+
+```
+$ npm install --save-dev eslint eslint-config-ego
+```
+
+## Usage
+
+Once the `eslint-config-ego` package is installed, you can use it by specifying `ego` in the [`extends`](http://eslint.org/docs/user-guide/configuring#extending-configuration-files) section of your [ESLint configuration](http://eslint.org/docs/user-guide/configuring).
+
+```js
+{
+  "extends": "ego",
+  "rules": {
+    // Additional, per-project rules...
+  }
+}
+```
+
+## License
+
+MIT © e.GO Digital GmbH
---
+++ index.js
@@ -0,0 +1,155 @@
+'use strict';
+
+// MIT License
+// 
+// Copyright (c) 2020 e.GO Digital GmbH, Aachen, Germany
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
+module.exports = {
+    "env": {
+        "browser": true,
+        "es6": true
+    },
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "project": "tsconfig.json",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint",
+        "unicorn",
+        "eslint-plugin-import",
+        "no-null"
+    ],
+    "rules": {
+        "camelcase": "off",
+        "@typescript-eslint/array-type": "warn",
+        "@typescript-eslint/await-thenable": "warn",
+        "indent": "off",
+        "@typescript-eslint/indent": ["warn", 4],
+        "@typescript-eslint/interface-name-prefix": [
+            "warn",
+            {
+                "prefixWithI": "always",
+            },
+        ],
+        "@typescript-eslint/member-delimiter-style": [
+            "warn",
+            {
+                "multiline": {
+                    "delimiter": "semi",
+                    "requireLast": true
+                },
+                "singleline": {
+                    "delimiter": "semi",
+                    "requireLast": false
+                }
+            }
+        ],
+
+        "@typescript-eslint/naming-convention": [
+            "warn",
+            {
+                "selector": "typeLike",
+                "format": ["PascalCase"]
+            },
+            {
+                "selector": "function",
+                "format": ["camelCase"]
+            },
+            {
+                "selector": "method",
+                "format": ["camelCase"]
+            },
+            {
+                "selector": "memberLike",
+                "modifiers": ["private", "protected"],
+                "format": ["camelCase"],
+                "leadingUnderscore": "require"
+            },
+            {
+                "selector": "enumMember",
+                "format": ["PascalCase"]
+            },
+            {
+                "selector": "variable",
+                "types": ["boolean"],
+                "format": ["PascalCase"],
+                "prefix": ["is", "should", "has", "can", "did", "will"]
+            },
+            {
+                "selector": "variable",
+                "format": ["camelCase", "UPPER_CASE"]
+            },
+            {
+                "selector": "typeParameter",
+                "format": ["PascalCase"],
+                "prefix": ["T"]
+            },
+            {
+                "selector": "parameter",
+                "format": ["camelCase"],
+                "leadingUnderscore": "allow"
+            },
+        ],
+
+        "@typescript-eslint/quotes": [
+            "warn",
+            "single"
+        ],
+        "@typescript-eslint/semi": [
+            "warn",
+            "always"
+        ],
+        "arrow-body-style": "warn",
+        "curly": "warn",
+        "eqeqeq": [
+            "warn",
+            "always"
+        ],
+        "id-blacklist": [
+            "warn",
+            "any",
+            "Number",
+            "number",
+            "String",
+            "string",
+            "Boolean",
+            "boolean",
+            "Undefined",
+            "undefined"
+        ],
+        "id-match": "warn",
+        "import/no-default-export": "warn",
+        "no-null/no-null": "off",
+        "no-redeclare": "warn",
+        "no-throw-literal": "warn",
+        "no-trailing-spaces": "warn",
+        "no-underscore-dangle": "warn",
+        "no-unused-expressions": "warn",
+        "no-var": "warn",
+        "spaced-comment": "warn",
+        "unicorn/filename-case": "warn",
+
+        "valid-jsdoc": "warn",
+        "no-irregular-whitespace": "warn",
+        "brace-style": "warn"
+    }
+};
